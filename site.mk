02dd97ce35149d57b064e16161314bb9cbfd79dc
Byggvir <thomas@arend-rhb.de>
2020-02-28 18:29:40 +0100

    Upadte for Gluon 2019.x

--- site.mk
+++ site.mk
@@ -1,6 +1,6 @@
 # --- Release Information
 
-DEFAULT_GLUON_RELEASE := v2018.2.4-$(shell date '+%Y%m%d')-stable
+DEFAULT_GLUON_RELEASE := v2019.1.2-$(shell date '+%Y%m%d')-stable
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
